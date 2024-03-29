b04dcde80231e49d83c70d4ee72bbb61711201a5
Jay <138824448+jaythemself@users.noreply.github.com>
2024-03-29 17:28:43 -0500

    initialized owt backend w spring

---
+++ owt-backend/src/test/java/com/owt/owtbackend/OwtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owt.owtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
