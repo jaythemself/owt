b75d56cb855f60af43f04e2e53c29e88f11d8182
Jay <138824448+jaythemself@users.noreply.github.com>
2024-04-06 21:44:31 -0500

    beginning structures for site data

---
+++ owt-backend/src/main/java/com/owt/owtbackend/models/data/siteRepository.java
@@ -0,0 +1,7 @@
+// package com.owt.owtbackend.models.data;
+
+// import  com.owt.owtbackend.models.site;
+// import org.springframework.data.repository.CrudRepository;
+
+// public interface siteRepository extends CrudRepository<Site, Integer>{
+// }
