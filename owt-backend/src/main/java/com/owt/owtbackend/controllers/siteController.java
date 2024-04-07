37ac30437bfc2fed2bfd1fb907d4b2ec013195bf
Jay <138824448+jaythemself@users.noreply.github.com>
2024-04-06 22:08:02 -0500

    set up structure for backend site data/models/controllers, notes for frontend/user handling

--- owt-backend/src/main/java/com/owt/owtbackend/controllers/siteController.java
+++ owt-backend/src/main/java/com/owt/owtbackend/controllers/siteController.java
@@ -1,1 +1,2 @@
-// set package,
+// set package, imports, and shell for adding, updating, deleting, displaying site info
+// set up sql and basic user authentication, refactor frontend to manage queries, api
