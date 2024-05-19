9834ca19a887071cc00ca110a83d0db5148d5df6
Jay <138824448+jaythemself@users.noreply.github.com>
2024-05-18 23:50:04 -0500

    abstract entity and appropriate dependencies

--- owt-backend/src/main/java/com/owt/owtbackend/models/abstractEntity.java
+++ owt-backend/src/main/java/com/owt/owtbackend/models/abstractEntity.java
@@ -1,34 +1,35 @@
-//package com.owt.owtbackend.models;
-//
-//import com.fasterxml.jackson.annotation.JsonTypeId;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.MappedSuperclass;
-//
-//import java.util.Objects;
-//
-//@MappedSuperclass
-//public abstract class AbstractEntity {
-//
-//    @Id
-//    @GeneratedValue(
-//            strategy= GenerationType.AUTO,
-//            generator="native"
-//    )
-//    private int id;
-//
-//    public int getId() {return id;}
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//
-//        AbstractEntity entity = (AbstractEntity) o;
-//        return id == entity.id;
-//    }
-//
-//    @Override
-//    public int hashCode() {return Objects.hash(id);}
-//}
+package com.owt.owtbackend.models;
+
+//potentially add in import com.fasterxml.jackson.annotation.JsonTypeId; ??
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class abstractEntity {
+
+    @Id
+    @GeneratedValue(
+            strategy= GenerationType.AUTO,
+            generator="native"
+    )
+    private int id;
+
+    public int getId() {return id;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        abstractEntity entity = (abstractEntity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {return Objects.hash(id);}
+}
