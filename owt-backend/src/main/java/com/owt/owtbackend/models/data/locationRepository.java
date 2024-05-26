de3db7cd55b5457b73bd7f165e9f634fd3d0918f
Jay <138824448+jaythemself@users.noreply.github.com>
2024-05-25 23:04:11 -0500

    location repository

---
+++ owt-backend/src/main/java/com/owt/owtbackend/models/data/locationRepository.java
@@ -0,0 +1,7 @@
+package com.owt.owtbackend.models.data;
+
+import  com.owt.owtbackend.models.location;
+import org.springframework.data.repository.CrudRepository;
+
+public interface locationRepository extends CrudRepository<location, Integer>{
+}
--- owt-backend/src/main/java/com/owt/owtbackend/models/data/siteRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.owt.owtbackend.models.data;
-
-// import  com.owt.owtbackend.models.site;
-// import org.springframework.data.repository.CrudRepository;
-
-// public interface siteRepository extends CrudRepository<Site, Integer>{
-// }
